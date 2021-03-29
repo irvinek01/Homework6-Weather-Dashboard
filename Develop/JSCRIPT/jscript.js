80065b9f2223d2984cd998e956775f457953cf2f
Arvin John Bautista <arvinyoung01@gmail.com>
2021-03-29 05:02:26 -0700

    Weather forecasts fully functional, lack of savedCity local storage

--- Develop/JSCRIPT/jscript.js
+++ Develop/JSCRIPT/jscript.js
@@ -1,64 +1,99 @@
 var form = document.querySelector("#form");
 var searchCity = document.querySelector("#searchCity");
 var searchResults = document.querySelector("#searchResults");
-
+var fiveDayForecast = document.querySelector("#fiveDayForecast");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    var weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "San Diego" + "&units=imperial&appid=d87a354ef5592a59d3994382f76cd854";
-    // var fiveDaywithUVIndex = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=d87a354ef5592a59d3994382f76cd854";
-    // Promise.all([
-    //     fetch(weatherAPIURL),
-    //     fetch(iconAPIURL),
-    // ])
-    //     .then(function (responses) {
-    //         return Promise.all(responses.map(function (response) {
-    //             return response.json();
-    //         }));
-    //     }).then(function (data) {
-    //         console.log(data);
-    //         printWeatherForecast(data);
-    //     }).catch(function (error) {
-    //         console.log(error);
-    //     });
+    var weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity.value + "&units=imperial&appid=d87a354ef5592a59d3994382f76cd854";
     fetch(weatherAPIURL)
-        .then(function (response) {
-            if (response.status === 404) {
+        .then(function (response1) {
+            if (response1.status === 404) {
                 alert("Cannot find " + searchCity.value);
-            } else if (response.status === 400) {
+                return;
+            } else if (response1.status === 400) {
                 alert("You did not enter any!");
+                return;
             }
-            return response.json();
+            return response1.json();
         })
         .then(function (weatherAPIURLdataObj) {
-            console.log(weatherAPIURLdataObj);
-            printWeatherForecast(weatherAPIURLdataObj);
+            printWeatherForecastToday(weatherAPIURLdataObj);
         });
 })
 
-var printWeatherForecast = function (results) {
-    searchResults.innerHTML = "";
+var printWeatherForecastToday = function (results) {
+    searchResults.innerHTML="";
+    fiveDayForecast.innerHTML="";
+    searchResults.innerHTML="CITY";
+    fiveDayForecast.innerHTML="5-DAY FORECAST";
     cityMainWeatherEl = document.createElement("h3");
     cityMainWeatherElIcon = document.createElement("img");
     cityMainTempEl = document.createElement("p");
     cityMainHumidEl = document.createElement("p");
     cityMainWindSpdEl = document.createElement("p");
 
-    cityMainWeatherEl.textContent = results.name + " ("+(moment.unix(results.dt).format("MMM DD YYYY"))+")";
+    cityMainWeatherEl.textContent = results.name + " (" + (moment.unix(results.dt).format("MMM DD YYYY")) + ")";
     cityMainWeatherElIcon.src = "http://openweathermap.org/img/wn/" + results.weather[0].icon + "@2x.png";
-    cityMainWeatherElIcon.width, cityMainWeatherElIcon.height  = "35";
+    cityMainWeatherElIcon.width, cityMainWeatherElIcon.height = "45";
 
-    cityMainTempEl.textContent = "Temperature: " + results.main.temp;
-    cityMainHumidEl.textContent = "Humidity Percentage: " + results.main.humidity + "%";
-    cityMainWindSpdEl.textContent = "Wind Speed: " + results.wind.speed;
-    
-    searchResults.appendChild(cityMainWeatherEl);
-    cityMainWeatherEl.appendChild(cityMainWeatherElIcon);
+    cityMainTempEl.textContent = "Temperature: " + results.main.temp + " °F";
+    cityMainHumidEl.textContent = "Humidity " + results.main.humidity + " %";
+    cityMainWindSpdEl.textContent = "Wind Speed: " + results.wind.speed + " MPH";
 
+    searchResults.appendChild(cityMainWeatherEl); cityMainWeatherEl.appendChild(cityMainWeatherElIcon);
     searchResults.appendChild(cityMainTempEl);
     searchResults.appendChild(cityMainHumidEl);
     searchResults.appendChild(cityMainWindSpdEl);
+    weatherForecast5Day(results.coord.lat, results.coord.lon);
 }
+function weatherForecast5Day(cityLat, cityLon) {
+    var oneCallAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon="
+        + cityLon + "&appid=d87a354ef5592a59d3994382f76cd854";
+    fetch(oneCallAPI)
+        .then(function (response2) {
+            return response2.json();
+        })
+        .then(function (forecast5DayObj) {
+            console.log(forecast5DayObj);
+            printWeatherForecast5Day(forecast5DayObj);
+        });
+}
+var printWeatherForecast5Day = function (results) {
+    cityMainUV = document.createElement("p");
+    cityMainUV.textContent = "UV Index: " + results.current.uvi;
+    searchResults.appendChild(cityMainUV);
+    dayForecastData = results.daily;    
+    fiveDayForecastHeader = document.createElement("div");
 
-// var weatherForecast5Day = function (results) {
-//     oneCallAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="{lat}"&lon="{lon}"&appid=d87a354ef5592a59d3994382f76cd854"
-// }
+    for (var i = 1; i <= 5; i++) {
+        
+        eachDayForecastDiv = document.createElement("div");
+        eachDayForecastDiv.classList.add("d-flex","eachDayForecast","p-1","m-1"); 
+        date = document.createElement("p");
+        dayWeatherIcon = document.createElement("img");
+        dayWeatherTemp = document.createElement("p");
+        dayWeatherHumid = document.createElement("p");
+        dayWeatherWind = document.createElement("p");
+        dayWeatherUVIndex = document.createElement("p");
+
+        date.textContent = (moment.unix(results.daily[i].dt).format("MMM DD YYYY"));
+        dayWeatherIcon.src = "http://openweathermap.org/img/wn/" + results.daily[i].weather[0].icon + "@2x.png";
+        dayWeatherIcon.width, dayWeatherIcon.height = "30";
+        dayWeatherTemp.textContent = "Temperature: " + results.daily[i].temp.day + " °F";
+        dayWeatherHumid.textContent = "Humidity " + results.daily[i].humidity + " %";
+        dayWeatherWind.textContent = "Wind Speed: " + results.daily[i].wind_speed + " MPH";
+        dayWeatherUVIndex.textContent = "UV Index: " + results.daily[i].uvi;
+
+        fiveDayForecast.appendChild(eachDayForecastDiv);
+        eachDayForecastDiv.appendChild(date);
+        eachDayForecastDiv.appendChild(dayWeatherIcon);
+        eachDayForecastDiv.appendChild(dayWeatherTemp);
+        eachDayForecastDiv.appendChild(dayWeatherHumid);
+        eachDayForecastDiv.appendChild(dayWeatherWind);
+        eachDayForecastDiv.appendChild(dayWeatherUVIndex);
+    }
+
+}
+function clearContentTextArea(element) {
+    element.value = "";
+  }
